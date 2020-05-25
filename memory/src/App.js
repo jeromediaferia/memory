b049abff50be4d60900023b4c0dd0d207a066842
Jérôme Diaferia <pro.jerome.diaferia@gmail.com>
2020-05-25 16:56:39 +0200

    Mise en place de l'event sur les cards

--- memory/src/App.js
+++ memory/src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
 import './App.css'
 
+import Card from './Card'
+import GuessCount from './GuessCount'
 class App extends Component {
+    // Ajout de l'écouteur sur le card
+    handleCardClick(card) {
+        console.log(card, 'clicked')
+    }
     render() {
-        return <div className="memory" />
+        return (
+            <div className="memory">
+                <GuessCount guesses={0} />
+                <Card card="😀" feedback="hidden" onClick={this.handleCardClick} />
+                <Card card="🎉" feedback="justMatched" onClick={this.handleCardClick} />
+                <Card card="💖" feedback="justMismatched" onClick={this.handleCardClick} />
+                <Card card="🎩" feedback="visible" onClick={this.handleCardClick} />
+                <Card card="🐶" feedback="hidden" onClick={this.handleCardClick} />
+                <Card card="🐱" feedback="justMatched" onClick={this.handleCardClick} />
+            </div>
+        )
     }
 }
 
-export default
+export default App
