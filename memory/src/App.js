71a74fcf06049e7ebd203c8af946056878180a18
Jérôme Diaferia <pro.jerome.diaferia@gmail.com>
2020-05-26 17:04:50 +0200

    Mise en place des propTypes

--- memory/src/App.js
+++ memory/src/App.js
@@ -1,23 +1,47 @@
 import React, { Component } from 'react'
+import shuffle from 'lodash.shuffle'
+
 import './App.css'
 
 import Card from './Card'
 import GuessCount from './GuessCount'
+import HallOfFame, {FAKE_HOF} from "./HallOfFame";
+
+const SIDE = 6
+const SYMBOLS = '😀🎉💖🎩🐶🐱🦄🐬🌍🌛🌞💫🍎🍌🍓🍐🍟🍿'
+
 class App extends Component {
-    // Ajout de l'écouteur sur le card
+    cards = this.generateCards()
+
+    generateCards() {
+        const result = []
+        const size = SIDE * SIDE
+        const candidates = shuffle(SYMBOLS)
+        while (result.length < size) {
+            const card = candidates.pop()
+            result.push(card, card)
+        }
+        return shuffle(result)
+    }
+
     handleCardClick(card) {
         console.log(card, 'clicked')
     }
+
     render() {
+        const won = new Date().getSeconds() % 2 === 0
         return (
             <div className="memory">
                 <GuessCount guesses={0} />
-                <Card card="😀" feedback="hidden" onClick={this.handleCardClick} />
-                <Card card="🎉" feedback="justMatched" onClick={this.handleCardClick} />
-                <Card card="💖" feedback="justMismatched" onClick={this.handleCardClick} />
-                <Card card="🎩" feedback="visible" onClick={this.handleCardClick} />
-                <Card card="🐶" feedback="hidden" onClick={this.handleCardClick} />
-                <Card card="🐱" feedback="justMatched" onClick={this.handleCardClick} />
+                {this.cards.map((card, index) => (
+                    <Card
+                        card={card}
+                        feedback="visible"
+                        key={index}
+                        onClick={this.handleCardClick}
+                    />
+                ))}
+                {won && <HallOfFame entries={FAKE_HOF} />}
             </div>
         )
     }
